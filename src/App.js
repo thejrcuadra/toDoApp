26ccaea6fa9e0666995684f3b07f190a96333406
jrc <jrc@josericardocuadra.com>
2025-02-19 09:17:01 -0500

    24:29 -- indexedDB not working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import './App.css'
+import { Dexie } from 'dexie'
+import { useLiveQuery } from 'dexie-react-hooks'
+
+const db = new Dexie('todoApp')
+db.version(1).stores({
+  todos: '++id, task,completed, date'
+})
+
+const { todos } = db
 
 const App = () => {
+  const allItems = useLiveQuery(() => todos.toArray(), [])
+
+  const addTask = async (event) => {
+    event.preventDefault()
+    const taskField = document.querySelector('#TaskInput')
+    
+    await todos.add({
+      task: taskField['value'],
+      completed: false
+    })
+
+    taskField['value'] = ''
+  }
+
   return (
     <div className="container">
       <h3 className="teal-text center-align">Todo App</h3>
-      <form className="add-item-form">
+      <form className="add-item-form" onSubmit={addTask}>
         <input
           type="text"
+          id="TaskInput"
           className="itemField"
           placeholder="What do you want to do today?"
           required
